5a3251cec70220a9beee22092613cf2b6d3c36b1
broma0 <121064585+broma0@users.noreply.github.com>
2025-10-27 15:11:48 -0400

    cvec/ext.h cvec/ext_tpl.h dvec/ext.h dvec/ext_tpl.h ext/tpl.h ext/tpl_para.h ivec/ext.h ivec/ext_tpl.h vec/tpl.h vec/tpl_para.h: update tk_parallel_sfx +17
    lib/santoku/cvec/ext.h
      - tk_cvec_bits_and, tk_cvec_bits_extend_mapped, tk_cvec_bits_from_ivec, tk_cvec_bits_hamming, tk_cvec_bits_hamming_mask, tk_cvec_bits_or, tk_cvec_bits_popcount, tk_cvec_bits_to_ascii, tk_cvec_bits_to_ivec, tk_cvec_bits_xor
      ~ tk_cvec_push_str
    lib/santoku/cvec/ext_tpl.h
      + tk_parallel_sfx
    lib/santoku/dvec/ext.h
      - tk_dvec_center, tk_dvec_rnorml2
    lib/santoku/dvec/ext_tpl.h
      + tk_parallel_sfx
    lib/santoku/ivec/ext.h
      - tk_ivec_bits_extend, tk_ivec_bits_extend_mapped, tk_ivec_bits_from_cvec
    lib/santoku/ivec/ext_tpl.h
      + tk_parallel_sfx
    lib/santoku/vec/ext/tpl.h
      - tk_vec_pfx
    lib/santoku/vec/ext/tpl_para.h
      + tk_parallel_sfx
    lib/santoku/vec/tpl.h
      - tk_vec_pfx
    lib/santoku/vec/tpl_para.h
      + tk_parallel_sfx, tk_vec_pfx
    
    also: doc/todo.md, lib/santoku/parallel/tpl.h, make.common.lua

---
+++ lib/santoku/dvec/ext_tpl.h
@@ -0,0 +1,36 @@
+
+
+
+#ifndef tk_parallel_sfx
+#error "Must include santoku/parallel/tpl.h before this template"
+#endif
+
+static inline void tk_parallel_sfx(tk_dvec_center) (
+  double *M,
+  size_t N,
+  size_t K
+) {
+
+  TK_PARALLEL_FOR(schedule(static))
+  for (size_t j = 0; j < K; j ++) {
+    double mu = 0.0;
+    for (size_t i = 0; i < N; i ++) mu += M[i * K + j];
+    mu /= (double)N;
+    for (size_t i = 0; i < N; i ++) M[i * K + j] -= mu;
+  }
+}
+
+static inline void tk_parallel_sfx(tk_dvec_rnorml2) (
+  double *M,
+  size_t N,
+  size_t K
+) {
+  TK_PARALLEL_FOR(schedule(static))
+  for (size_t i = 0; i < N; i++) {
+    double *row = M + i * K;
+    double norm = cblas_dnrm2(K, row, 1);
+    if (norm > 0.0) {
+      cblas_dscal(K, 1.0 / norm, row, 1);
+    }
+  }
+}
